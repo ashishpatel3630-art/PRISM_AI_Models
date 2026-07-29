e6518fb52cb191e2155138ad878756c6f70cda47
Ashish Patel <ashishpatel3630@gmail.com>
2026-07-29 14:10:41 +0530

    churn ui completed

--- PRISM_AI/frontend/vite.config.js
+++ PRISM_AI/frontend/vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+/** @type {import('tailwindcss').Config} */
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+        tailwindcss(),
+
+  ],
 
   server: {
     port: 5174,
+  },
+   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
 })
